37e603757cfcde82b5160827a1fea74a18e3a0b5
go2maxi <mishel2002@hotmail.com>
2026-01-20 17:52:00 +0900

    Add basic functions practice script

---
+++ functions_basic.py
@@ -0,0 +1,11 @@
+# functions_basic.py
+# Basic function definitions in Python
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+print(greet("Python"))
+print(add(3, 5))
