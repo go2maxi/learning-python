d5ee721249be2d0d2ae1647dd606b863cee86c20
go2maxi <mishel2002@hotmail.com>
2026-01-20 17:48:16 +0900

    Add conditionals practice script

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+# conditionals.py
+# Basic conditional statements in Python
+
+age = 20
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+score = 85
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+else:
+    print("Grade: C")
